9e777c0292a8421a88c003d25f7212feb260d75a
Deepti Hegde <deeptibhegde@gmail.com>
2023-03-13 18:06:10 -0400

    Delete dist_train_BERT.sh

--- scripts/dist_train_BERT.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-NGPUS=$1
-PORT=$2
-PY_ARGS=${@:3}
-
-python -m torch.distributed.run --master_port=${PORT} --nproc_per_node=${NGPUS} main_BERT.py --launcher pytorch --sync_bn ${PY_ARGS}
